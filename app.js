4408f078e7faaef9e15ab562b057a3e9fa58a58c
anshullk <anshullkashyaph@gmail.com>
2023-04-23 19:35:12 +0530

    Allow users to submit a secret

--- app.js
+++ app.js
@@ -4,76 +4,43 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-// the order should not be messed up as one thing depends on another to work.
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-//"passport-local" will be one of the dependencies that will be required by "passport-local-mongoose"
-//so, no need to require it explicitly.
+
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-//INITIALIZE THE MIDDLEWARE
+
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
 }))
-// This is the basic express session({..}) initialization.
-app.use(passport.initialize())  // "init passport" on every route call. The method ".initialize()" come already bundled with the passport and this would initialize passport for us to use it.
-//passport.initialize assigns _passport object to request object
-app.use(passport.session())     // allows "passport" to use "express-session".
+app.use(passport.initialize())  
+app.use(passport.session())     
 
 
 const connDB = mongoose.connect("mongodb://127.0.0.1:27017/userDB")
 /////////////////////////////////////////////////////////  CODE  //////////////////////////////////////////////////////////////////////
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    secret: String               //<--Ammend the schema to store users secrets
 })
-//To use "passport-local-schema" you first need to plugin Passport-Local Mongoose into your User schema
-//Passport-Local Mongoose will add a username, hash and salt field to store the username, the hashed password and the salt value to the database.
+
 userSchema.plugin(passportLocalMongoose);
-//This will enable the plugin to the schema and is ready to be used now.
-//Passport-Local Mongoose is a Mongoose plugin that simplifies building username and password login with Passport.
+
 
 const User = new mongoose.model("User", userSchema)
-//Define an authentication strategy
+
 passport.use(User.createStrategy());
 
-//Serialize and De-Serialize (authenticated) users
-passport.serializeUser(User.serializeUser()); //persist user data (after successful authentication) into session
-/*
--------------------------
-WHAT DOES SERIALIZE USER MEAN?
-1. "express-session" creates a "req.session" object, when it is invoked via app.use(session({..}))
-2. "passport" then adds an additional object "req.session.passport" to this "req.session".
-3. All the serializeUser() function does is,
-"receives" the "authenticated user" object from the "Strategy" framework, and attach the authenticated user to "req.session.passport.user.{..}"
-In above case we receive {id: 123, name: "Kyle"} from the done() in the authUser function in the Strategy framework, 
-so this will be attached as 
-req.session.passport.user.{id: 123, name: "Kyle"}
-3. So in effect during "serializeUser", the PassportJS library adds the authenticated user to end of the "req.session.passport" object.
-This is what is meant by serialization.
-This allows the authenticated user to be "attached" to a unique session. 
-This is why PassportJS library is used, as it abstracts this away and directly maintains authenticated users for each session within the "req.session.passport.user.{..}"
----------------------------- */
+passport.serializeUser(User.serializeUser()); 
+
 passport.deserializeUser(User.deserializeUser());
-/*----------------
-Now anytime we want the user details for a session, we can simply get the object that is stored in “req.session.passport.user.{..}”.
-WHAT DOES DE-SERIALIZE USER MEAN?
-1. Passport JS conveniently populates the "userObj" value in the deserializeUser() with the object attached at the end of "req.session.passport.user.{..}"
-2. When the done (null, user) function is called in the deserializeUser(), Passport JS takes this last object attached to "req.session.passport.user.{..}", and attaches it to "req.user" i.e "req.user.{..}"
-In our case, since after calling the done() in "serializeUser" we had req.session.passport.user.{id: 123, name: "Kyle"}, 
-calling the done() in the "deserializeUser" will take that last object that was attached to req.session.passport.user.{..} and attach to req.user.{..} 
-i.e. req.user.{id: 123, name: "Kyle"}
-3. So "req.user" will contain the authenticated user object for that session, and you can use it in any of the routes in the Node JS app. 
-eg. 
-app.get("/dashboard", (req, res) => {
-res.render("dashboard.ejs", {name: req.user.name})
-}) 
-*/
+
 app.get("/", function (req, res) {
     res.render("home");
 });
@@ -86,18 +53,18 @@ app.get("/login", function (req, res) {
     res.render("login");
 });
 app.get("/secrets", function (req, res) {
-    //check if the user is authenticated
-    //this is where we will be relying on our passport, expres-session, passport-local,passport-local-mongoose. 
-    //"req.isAuthenticated()" function allow us to protect routes that can be accessed only after a user logs in.
-    if (req.isAuthenticated()) {    //<--returns “true” in case an authenticated user is present in “req.session.passport.user” and returns “false” in case no authenticated user is present in “req.session.passport.user
-        res.render("secrets");
-    } else {
-        res.redirect("/login");
-    }
-});
-//Now, here is when we want to de-authenticate the user and ends the session.
+    //find all the users in the database where "secret" field is "not equal" to "none"
+    User.find({"secret":{$ne:null}}).then(function(result){    
+
+        res.render("secrets",{usersWithSecrets:result})
+
+    }).catch(function(err){
+        console.log(err);
+    })
+ });
+
 app.get("/logout", function (req, res) {
-    req.logout(function (err) {  //<-- provided to us by the passport js
+    req.logout(function (err) {  
         if (err) {
             console.log(err);
         } else {
@@ -106,12 +73,15 @@ app.get("/logout", function (req, res) {
     });
 
 })
-//Authenticate the user using ".register()" method provided by the passport-local-mongoose package.
-//It's only because of passport-local-mongoose package we don't have to create a user, .save() a user and interact with the mongoose databae directly.
-/*const User = mongoose.model('Users', UserSchema);
-User.register({username:'username', active: false}, 'password', function(err, user) {
-  if (err) { ... } 
-  */
+
+app.get("/submit",function(req,res){
+    if (req.isAuthenticated()){
+        res.render("submit");
+    }else{
+        res.redirect("/login");
+    }
+})
+
 app.post("/register", function (req, res) {
     User.register({ username: req.body.username }, req.body.password, function (err, user) {
         if (err) {
@@ -129,8 +99,7 @@ app.post("/login", function (req, res) {
         username: req.body.username,
         password: req.body.password
     })
-    //the ".login()" function comes from passport
-    //search the user in the database
+    
     req.login(user, function (err) {
         if (err) {
             console.log(err)
@@ -141,6 +110,24 @@ app.post("/login", function (req, res) {
             })
         }
     })
+})
+
+app.post("/submit",function(req,res){
+    const submittedSecret=req.body.secret;
+    //Find the user in the database and save the secret into their file
+    console.log(req.user.id) //<-- As the "passport" helps us to save the user details into the "req" variable whenever a new "login session" is initiated.
+    User.findById(req.user.id).then(function(result){
+        result.secret=submittedSecret;
+        result.save().then(function(){
+            res.redirect("/secrets")
+        }).catch(function(err){
+            console.log(err);
+        })
+        
+    }).catch(function(err){
+        console.log(err);
+    })
+
 })
 connDB.then(function () {
     console.log("Successfully connected to the mongoDB database")
